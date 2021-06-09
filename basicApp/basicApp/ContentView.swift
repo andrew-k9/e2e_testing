82739f1deb0aaa8868adba6d9d99f18b8204745d
andrew-k9 <lt.kvothe@gmail.com>
2021-06-09 11:40:38 -0400

    Basic iOS app

---
+++ basicApp/basicApp/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  basicApp
+//
+//  Created by Andrew Kloecker on 6/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var saying: String = "hey"
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Hello Selenium")
+            Text("\(saying)")
+            Button(action:{
+                self.saying += "y"
+            }, label: {
+                Text("Add y")
+            })
+        }.padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
---
+++ basicApp/basicApp/basicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  basicAppApp.swift
+//  basicApp
+//
+//  Created by Andrew Kloecker on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct basicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
